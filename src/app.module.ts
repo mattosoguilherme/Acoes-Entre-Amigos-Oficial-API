51c553e3fd5150e014db2549f0b2ea708af985f7
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-06-11 13:00:25 -0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RaffleModule } from './raffle/raffle.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, RaffleModule],
   controllers: [AppController],
   providers: [AppService],
 })

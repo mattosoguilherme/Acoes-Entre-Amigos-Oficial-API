59cdacffcfb21d6a5bee3688dd6f446df89bcf33
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-07-05 00:32:05 -0300

    commit

--- src/raffle/dto/create-raffle.dto.ts
+++ src/raffle/dto/create-raffle.dto.ts
@@ -1,82 +1,120 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsArray, IsDateString, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  validateOrReject,
+} from 'class-validator';
 
 export class CreateRaffleDto {
-  
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  title: string;
+  @ApiProperty({
+    default: 'Chá Rifa',
+    description: 'Campo designado ao titulo da rifa',
+  })
+  titulo: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  frontCover: string;
+  @ApiProperty({
+    default: 1,
+    description:
+      ' Neste campo será inserida o ID de um categoria pré cadastrada no sistema',
+  })
+  categoria: number;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  description: string;
+  @ApiProperty({ default: 'Rua Tenente Manoel Alves dos Anjos' })
+  local: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  place: string;
+  @ApiProperty({
+    description: 'Este campo deverá ser preenchido com a data de encerramento',
+  })
+  prazo_expiracao: Date;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  support_contact: string;
+  @ApiProperty({
+    description: 'Este campo deverá ser preenchido com a data de sorteio',
+  })
+  dia_sorteio: Date;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  group_link: string;
+  @ApiProperty({
+    default: 10,
+    description:
+      'Este campo deverá ser preenchido com a quantidade cotas(números)',
+  })
+  cotas: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  limit_participant_quota: number;
+  @ApiProperty({
+    default: 10.5,
+    description:
+      'Este campo deverá ser preenchido com preço individual das cotas(números)',
+  })
+  preco_cota: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  price_quote: number;
+  @ApiProperty({
+    default: 3,
+    description:
+      'Este campo deverá ser preenchido com o limite de cotas(números) por comprador(a)',
+  })
+  limite_cota_user: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  quota_payment_term: number;
+  @ApiProperty({
+    default: '11992767398',
+    description:
+      'Este campo deverá ser preenchido com o telefone do organizador',
+  })
+  telefone_suporte: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  idCategory:number;
+  @ApiProperty({
+    default: 'Chá rifa do Gael, para comemorar um ano do nosso príncipe',
+    description: 'Este campo deverá ser preenchido com a descrição',
+  })
+  descricao: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  quantity_quotas:number;
+  @ApiProperty({
+    default:
+      'https://cdn.rifa.link/rifas/00000121757_rifas_7HbC8FBGwhqWdTxcXp6t.jpg',
+    description:
+      'Este campo deverá ser preenchido com uma imagem do prêmio ou folder desenvolvido pela nossa equipe',
+  })
+  img_premio: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  reservation_requirements: string[];
+  @ApiProperty({
+    default: 'whatsapp',
+    description:
+      'Este campo deverá ser preenchido com uma imagem do prêmio ou folder desenvolvido pela nossa equipe',
+  })
+  link_grupo: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  prizes: string[];
+  @ApiProperty({
+    default: 2,
+    description:
+      'Este campo deverá ser preenchido com um ID de um tipo pré cadastrado no sistema',
+  })
+  Tipo_rifa: number;
 
-  @IsArray()
-  @IsNotEmpty()
-  @ApiProperty()
-  promotions: object[];
+  @ApiProperty({
+    default: [1],
+    description:
+      'Este campo deverá ser preenchido com um ID de um tipo pré cadastrado no sistema',
+  })
+  Tipo_exibicao: number[];
 
-  @IsDateString()
-  @IsNotEmpty()
-  @ApiProperty()
-  term:Date;
+  @ApiProperty({
+    default: [1, 2, 3],
+    description:
+      'Este campo deverá ser preenchido com um IDs de requisitos pré cadastrados no sistema',
+  })
+  Requisitos_reserva: number[];
 
+  @ApiProperty({
+    default: ['celular', 'moto', 'fogão'],
+    description: 'Este campo deverá ser preenchido com os prêmios',
+  })
+  premios: string[];
 
+  @ApiProperty({
+    default:{"chave":"valor", "chave2":"valor2"},
+    description:
+      'Este campo deverá ser preenchido com as promoções de compra, se houver',
+  })
+  promocoes: JSON;
 }
